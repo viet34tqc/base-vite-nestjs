717ba660104a99bdef125e59f6793ce61dcaf2a7
viet34tqc <viet34tqc@gmail.com>
2025-06-21 13:38:19 +0700

    Make it run

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  'client/**/*.{js,jsx,ts,tsx}': [
+  'packages/client/**/*.{js,jsx,ts,tsx}': [
     'pnpm --filter client lint',
     'pnpm --filter client format',
   ],
-  'server/**/*.{js,ts}': [
+  'packages/server/**/*.{js,ts}': [
     'pnpm --filter server lint',
     'pnpm --filter server format',
   ],
