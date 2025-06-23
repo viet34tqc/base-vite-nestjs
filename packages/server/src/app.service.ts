60d90afe1234e95bc2355df7e7658c452d0ff4fe
viet34tqc <viet34tqc@gmail.com>
2025-06-23 21:09:44 +0700

    Using watch

--- packages/server/src/app.service.ts
+++ packages/server/src/app.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-
 @Injectable()
 export class AppService {
   constructor(private readonly prisma: PrismaService) {}
 
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello Worl@@d!';
   }
 
   async getAllExamples() {
